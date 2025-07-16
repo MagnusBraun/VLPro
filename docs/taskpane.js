d8189302b96c163c11f70bb9f7f9edbae5c78bdc
MagnusBraun <magnus.braun04@gmail.com>
2025-07-16 14:33:30 +0200

    Update taskpane.js

--- docs/taskpane.js
+++ docs/taskpane.js
@@ -5,7 +5,6 @@ Office.onReady(() => {
     console.log("🔥 taskpane.js geladen – aktuelle Version");
   }
 });
-
 const apiUrl = "https://vlp-upload.onrender.com/process";
 const storageKey = "pmfusion-column-mapping";
 
